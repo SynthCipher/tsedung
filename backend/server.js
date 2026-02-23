8cbbc35b49ffb55f4c56b86dc915bf9e72c19407
jigmat dorjey <jigmatdorjey255@gmail.com>
2026-02-23 22:57:18 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "https://jigmatdorjey.vercel.app",
-  "https://tsedung.vercel.app/",
+  "https://tsedung.vercel.app",
 
 ];
 
